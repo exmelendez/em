15d3011f68c1367f77ce2409dfa07d13d6a7c70f
Eddie Melendez <exmelendez@gmail.com>
2019-03-19 18:51:19 -0400

    collapse JS - 3/19/19

---
+++ ghu/js/collapse.js
@@ -0,0 +1,39 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+//for read more section
+const btn = document.getElementById("more-bio-btn");
+
+btn.addEventListener("click", () => { 
+  let btnVal = document.getElementById("more-bio-btn").value;
+  let moreText = document.getElementById("full-bio-p");
+  let readMoreSection = document.getElementById("r-more");
+  let readLessSection = document.getElementById("r-less");
+  
+  if (btnVal === "active") {
+      document.getElementById("more-bio-btn").value = "not-active";
+      // btn.innerHTML = "Read less"; 
+      readLessSection.style.display = "inline";
+      readMoreSection.style.display = "none";
+      moreText.style.display = "inline";
+  
+  } else {
+      document.getElementById("more-bio-btn").value = "active";
+      // btn.innerHTML = "Read more"; 
+      readLessSection.style.display = "none";
+      readMoreSection.style.display = "inline";
+      moreText.style.display = "none";
+  }
+});
