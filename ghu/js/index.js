b7acfa438c218e0a1d90e4774e82a83c78e75e3e
Eddie Melendez <exmelendez@gmail.com>
2019-03-23 21:44:02 -0400

    Create index.js

---
+++ ghu/js/index.js
@@ -0,0 +1,11 @@
+const bishopBioFeatureSection = document.getElementById("pastor-feature-bio");
+
+const bishopImgFeatureSection = document.getElementById("pastor-feature-img");
+
+bishopBioFeatureSection.addEventListener ("click", () => {
+  window.location.href = '../about.html#bishop-bio-container';
+});
+
+bishopImgFeatureSection.addEventListener ("click", () => {
+  window.location.href = '../about.html#bishop-bio-container';
+});
